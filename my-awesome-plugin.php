4a4f8832c42e03d717916559c97aa7a56a52211d
bhavan-mandy <bhavan.vitaldesign@gmail.com>
2025-10-24 01:28:14 +0530

    Fix the update 1.0.6

--- my-awesome-plugin.php
+++ my-awesome-plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name:           My Awesome Plugin
  * Plugin URI:            https://github.com/mandytechnologies/my-awesome-plugin
  * Description:           This is plugin to demonstrate how to create a WordPress plugin that can update itself from a GitHub repository.
- * Version:               1.0.5
+ * Version:               1.0.6
  * Requires PHP:          8.0
  * Requires at least:     6.1.0
  * Tested up to:          6.8.2
